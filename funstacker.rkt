3dc91a321fa61a5377646b7b96018cb39765fd14
pfe1223 <patrickester@APP-1554.local>
2017-04-25 13:39:25 -0700

    Finish Funstacker
    Finish the fun stacker activity.

--- funstacker.rkt
+++ funstacker.rkt
@@ -1,1 +1,27 @@
-#lang br
+#lang br/quicklang
+
+(define (read-syntax path port)
+  (define args (port->lines port))
+  (define handle-datums (format-datums '~a args))
+  (define module-datum `(module funstacker-mod "funstacker.rkt"
+                          (handle-args ,@handle-datums)))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
+
+(define-macro (funstacker-module-begin HANDLE-ARGS-EXPR)
+  #'(#%module-begin
+     (display (first HANDLE-ARGS-EXPR))))
+(provide (rename-out [funstacker-module-begin #%module-begin]))
+
+(define (handle-args . args)
+  (for/fold ([stack-acc empty])
+            ([arg (filter-not void? args)])
+    (cond
+      [(number? arg) (cons arg stack-acc)]
+      [(or (equal? * arg) (equal? + arg))
+       (define op-result
+         (arg (first stack-acc) (second stack-acc)))
+       (cons op-result (drop stack-acc 2))])))
+(provide handle-args)
+
+(provide + *)
